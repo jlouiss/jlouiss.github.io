0eaa7e7792f56b4d9646f7dde4e67173b25b8aec
Jean Louis Salbego <jlsalbego@protonmail.com>
2018-10-03 20:43:56 +0200

    Updates

---
+++ precache-manifest.e0c31c775566a34345ad836082426195.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "e00300a1bc6818904dd9",
+    "url": "/static/js/main.e00300a1.chunk.js"
+  },
+  {
+    "revision": "14e18f22037e80279980",
+    "url": "/static/js/1.14e18f22.chunk.js"
+  },
+  {
+    "revision": "e00300a1bc6818904dd9",
+    "url": "/static/css/main.443dc66e.chunk.css"
+  },
+  {
+    "revision": "4cb060d9b20fe3c3112e177fc6e4b4a5",
+    "url": "/index.html"
+  }
+];
